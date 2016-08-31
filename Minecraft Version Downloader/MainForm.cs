0af632829d0956aaf7e51776b40384aeb3553a99
TheWalker113 <radcraftplay@gmail.com>
2016-08-31 18:18:34 +0200

    [Fixed] After downloading X files in a row were displayed X MessageBoxes

--- Minecraft Version Downloader/MainForm.cs
+++ Minecraft Version Downloader/MainForm.cs
@@ -44,8 +44,9 @@ namespace Minecraft_Version_Downloader
 
 		private void MainForm_Load(object sender, EventArgs e)
 		{
-
-		}
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+        }
 
 		private void refreshButton_Click(object sender, EventArgs e)
 		{
@@ -97,8 +98,6 @@ namespace Minecraft_Version_Downloader
 				sfd.FileName = "minecraft_server";
 				if (sfd.ShowDialog() == DialogResult.OK)
 				{
-					client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
-					client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
 					string Url = versionServerUrl(metroListView1.SelectedItems[0].Text);
 					client.DownloadFileAsync(new Uri(Url), sfd.FileName);
 				}
