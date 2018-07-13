b9bb43c6b3b696e9e285ad14c9bf6b14ca8c0418
Distroir <radcraftplay2@gmail.com>
2018-07-13 22:13:51 +0200

    Added DownloadVersionsDialog

---
+++ Minecraft Version Downloader/DownloadVersionsDialog.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Minecraft_Server_Downloader
+{
+    partial class DownloadVersionsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroProgressBar1.Location = new System.Drawing.Point(23, 84);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(487, 23);
+            this.metroProgressBar1.TabIndex = 0;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 110);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(135, 20);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Downloading x of y...";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton1.Location = new System.Drawing.Point(435, 155);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Cancel";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // DownloadVersionsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 201);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Name = "DownloadVersionsDialog";
+            this.Text = "Fetching version list...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
---
+++ Minecraft Version Downloader/DownloadVersionsDialog.cs
@@ -0,0 +1,20 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Minecraft_Server_Downloader
+{
+    public partial class DownloadVersionsDialog : MetroForm
+    {
+        public DownloadVersionsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
