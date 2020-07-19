d51ac491559f377f211c488c727bb45bdf0c78a7
RadCraftplay <radcraftplay2@gmail.com>
2020-07-19 16:47:28 +0200

    Added AsyncStringDownloader

---
+++ Minecraft Version Downloader/Core/Downloaders/AsyncDownloadProgress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Server_Downloader.Core.Downloaders
+{
+    public class AsyncDownloadProgress
+    {
+        public AsyncDownloadProgress(int completed, int all)
+        {
+            Completed = completed;
+            All = all;
+        }
+
+        public int Completed { get; }
+        public int All { get; }
+    }
+}
---
+++ Minecraft Version Downloader/Core/Downloaders/AsyncStringDownloader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using Minecraft_Server_Downloader.Core.Downloaders;
+
+namespace Minecraft_Server_Downloader.Core.VersionListDownloaders
+{
+    public class AsyncStringDownloader
+    {
+        private readonly ImmutableArray<string> _downloadQueue;
+        private readonly HttpClient _client;
+        private readonly CancellationToken _token;
+
+        public AsyncStringDownloader(IEnumerable<string> downloadQueue, CancellationToken token)
+        {
+            _downloadQueue = ImmutableArray.CreateRange(downloadQueue);
+            _client = new HttpClient();
+        }
+
+        public async Task<ImmutableList<string>> DownloadList(IProgress<AsyncDownloadProgress> progress)
+        {
+            var downloadedVersions = new List<string>();
+            int completed = 0;
+
+            foreach (var url in _downloadQueue)
+            {
+                if (_token.IsCancellationRequested)
+                    break;
+
+                var downloadedString = await _client.GetStringAsync(url);
+                downloadedVersions.Add(downloadedString);
+
+                progress.Report(new AsyncDownloadProgress(++completed, _downloadQueue.Count()));
+            }
+
+            return ImmutableList.CreateRange(downloadedVersions);
+        }
+    }
+}
