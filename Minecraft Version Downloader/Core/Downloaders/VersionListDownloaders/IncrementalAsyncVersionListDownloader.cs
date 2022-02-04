0166271f06718566da56d8124310fa9a424cdd9c
RadCraftplay <radcraftplay2@gmail.com>
2022-02-04 22:58:51 +0100

    Added IncrementalAsyncVersionListDownloader

--- Minecraft Version Downloader/Core/Downloaders/VersionListDownloaders/IncrementalAsyncVersionListDownloader.cs
+++ Minecraft Version Downloader/Core/Downloaders/VersionListDownloaders/IncrementalAsyncVersionListDownloader.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Minecraft_Server_Downloader.Core.Downloaders.VersionInfoFileListDownloaders;
 using Minecraft_Server_Downloader.Structures;
 using Newtonsoft.Json;
 
@@ -12,17 +13,24 @@ namespace Minecraft_Server_Downloader.Core.Downloaders.Incremental
     public class IncrementalAsyncVersionListDownloader : IAsyncVersionListDownloader
     {
         private readonly AsyncStringDownloader _downloader;
-        private readonly List<VersionInfoFile> _localVersions;
+        private readonly IVersionFileListDownloader _versionListFileDownloader;
 
-        public IncrementalAsyncVersionListDownloader(CancellationToken token, List <VersionInfoFile> localVersions)
+        public IncrementalAsyncVersionListDownloader(CancellationToken token, List<VersionInfoFile> localVersions)
         {
             _downloader = new AsyncStringDownloader(token);
-            _localVersions = localVersions;
+            _versionListFileDownloader = new IncrementalVersionListDownloader(
+                _downloader, 
+                localVersions.Select(version => version.id).ToList());
         }
         
         public async Task<IEnumerable<VersionInfoFile>> DownloadListOfVersions(IProgress<AsyncDownloadProgress> progress)
         {
-            throw new NotImplementedException();
+            var versions = await _versionListFileDownloader.GetVersionInfoFileUrls();
+            var versionInfoFileUrls = versions.Select(version => version.url);
+            var versionInfoFileContents = await _downloader.DownloadList(versionInfoFileUrls, progress);
+            return versionInfoFileContents
+                .Select(JsonConvert.DeserializeObject<VersionInfoFile>)
+                .Where(info => info.downloads.server != null);
         }
     }
 }
