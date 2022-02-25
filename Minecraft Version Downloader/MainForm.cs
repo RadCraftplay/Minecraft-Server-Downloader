83e0a8526adbec59e82f93e12a96e3030288003c
RadCraftplay <radcraftplay2@gmail.com>
2022-02-26 00:15:01 +0100

    Integrated facade into UI

--- Minecraft Version Downloader/MainForm.cs
+++ Minecraft Version Downloader/MainForm.cs
@@ -20,17 +20,13 @@
 */
 using MetroFramework;
 using MetroFramework.Forms;
-using Minecraft_Server_Downloader.Core.Storage;
 using Minecraft_Server_Downloader.Structures;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Windows.Forms;
+using Minecraft_Server_Downloader.Core;
 using Minecraft_Server_Downloader.Core.Downloaders;
-using Minecraft_Server_Downloader.Core.Downloaders.FileDownloaders;
 
 namespace Minecraft_Server_Downloader
 {
@@ -38,45 +34,54 @@ namespace Minecraft_Server_Downloader
 	{
         #region Variables
 
-        /// <summary>
-        /// Downloader used for downloading server
-        /// </summary>
-        private IAsyncFileDownloader downloader;
-
-        private IProgress<AsyncDownloadProgress> progress;
-
-        /// <summary>
-        /// File containing list of Minecraft server versions
-        /// </summary>
-        string VersionListFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Distroir", "Minecraft Version Downloader", "server_versions.txt");
-
-        /// <summary>
-        /// Full, unfiltered list of server versions
-        /// </summary>
-        private List<VersionInfoFile> serverVersions = new List<VersionInfoFile>();
-
-        private readonly ILocalStorage versionStorage;
+        private readonly MinecraftServerDownloader _facade;
+        private readonly IProgress<AsyncDownloadProgress> _progress;
 
         #endregion
 
         public MainForm()
 		{
 			InitializeComponent();
-            versionStorage = new TextStorage(VersionListFilePath);
-            progress = new Progress<AsyncDownloadProgress>(DownloadProgressChanged);
-            downloader = new WebAsyncFileDownloader(progress);
+			_facade = new MinecraftServerDownloader();
+            _progress = new Progress<AsyncDownloadProgress>(DownloadProgressChanged);
 		}
 
         #region Events
+        
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+	        await _facade.Init();
+	        UpdateListOfVersions(await _facade.GetAllLocalServerVersions());
+        }
+        
+        private async void FilterCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+	        var selectedVersions = new List<string>();
+
+	        foreach (Control control in serverVersionFiltersGroupBox.Controls)
+	        {
+		        if (!(control is CheckBox checkBox) || checkBox.Tag == null || checkBox.Checked == false)
+			        continue;
+
+		        var selectedType = (string)checkBox.Tag;
+		        selectedVersions.Add(selectedType);
+	        }
+
+	        var serverVersions = await _facade
+		        .GetLocalServerVersionsThat(version => selectedVersions
+			        .Contains(version.type));
+            
+	        UpdateListOfVersions(serverVersions);
+        }
 
         #region Button events
 
         /// <summary>
         /// Refreshes list of minecraft server versions
         /// </summary>s
-        private void refreshButton_Click(object sender, EventArgs e)
-		{
-			var d = new DownloadVersionsDialog(serverVersions);
+        private async void refreshButton_Click(object sender, EventArgs e)
+        {
+	        var d = new DownloadVersionsDialog(_facade);
 			d.ShowDialog();
 
 			if (d.DialogCloseReason == CloseReason.Error)
@@ -86,14 +91,12 @@ namespace Minecraft_Server_Downloader
 			}
 			else if (d.DialogCloseReason == CloseReason.DownloadingFinished)
 			{
-                // Save version list
-                SaveVersionListFile(d.ServerVersions.ToList());
-                // Reload version list
-                LoadVersionListFile();
+				// Reload version list
+                UpdateListOfVersions(await _facade.GetAllLocalServerVersions());
 			}
 		}
 
-        private void downloadButton_Click(object sender, EventArgs e)
+        private async void downloadButton_Click(object sender, EventArgs e)
         {
             if (metroListView1.SelectedItems.Count == 0)
                 MessageBox.Show("Nothing selected!");
@@ -109,20 +112,18 @@ namespace Minecraft_Server_Downloader
 
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
-                    VersionInfoFile info = (VersionInfoFile)metroListView1.SelectedItems[0].Tag;
+	                var versionId = metroListView1.SelectedItems[0].Text;
 
                     downloadButton.Text = "Cancel";
                     downloadButton.Click -= downloadButton_Click;
                     downloadButton.Click += cancelButton_Click;
-
-                    downloader.DownloadFileAsync(info.downloads.server.url, sfd.FileName);
                 }
             }
         }
 
-        private async void cancelButton_Click(object sender, EventArgs e)
+        private void cancelButton_Click(object sender, EventArgs e)
         {
-	        await downloader.CancelAsync();
+	        _facade.CancelDownloadingServer();
 	        
 	        downloadButton.Text = "Download";
 	        downloadButton.Click -= cancelButton_Click;
@@ -135,114 +136,7 @@ namespace Minecraft_Server_Downloader
 
         #endregion
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-	        LoadVersionListFile();
-        }
-
-        private void FilterCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            List<string> selectedVersions = new List<string>();
-
-            foreach (Control control in serverVersionFiltersGroupBox.Controls)
-            {
-                if (control is CheckBox checkBox)
-                {
-                    if (checkBox.Tag == null || checkBox.Checked == false)
-                        continue;
-
-                    string selectedType = (string)checkBox.Tag;
-                    selectedVersions.Add(selectedType);
-                }
-            }
-
-            var lvItems = from version in serverVersions
-                          where selectedVersions.Contains(version.type)
-                          select CreateListViewItem(version);
-
-            metroListView1.Clear();
-            foreach (var item in lvItems)
-            {
-                metroListView1.Items.Add(item);
-            }
-        }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Checks if program data directories exist
-        /// </summary>
-        void checkDirectories()
-		{
-			if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Distroir")))
-				Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Distroir"));
-			if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Distroir", "Minecraft Version Downloader")))
-				Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Distroir", "Minecraft Version Downloader"));
-		}
-
-        /// <summary>
-        /// Saves version list file
-        /// </summary>
-        void SaveVersionListFile(List<Structures.VersionInfoFile> versions)
-        {
-            checkDirectories();
-            versionStorage.Save(versions);
-        }
-
-        /// <summary>
-        /// Loads version list file from disc
-        /// </summary>
-        void LoadVersionListFile()
-        {
-            if (!File.Exists(VersionListFilePath))
-                return;
-
-            // Clear lists
-            metroListView1.Items.Clear();
-            serverVersions.Clear();
-
-            foreach (VersionInfoFile versionFile in versionStorage.Load())
-            {
-                AddVersionToListView(versionFile);
-                serverVersions.Add(versionFile);
-            }
-        }
-
-        private void AddVersionToListView(VersionInfoFile info)
-        {
-            metroListView1.Items.Add(CreateListViewItem(info));
-        }
-
-        private ListViewItem CreateListViewItem(VersionInfoFile info)
-        {
-            return new ListViewItem(info.id)
-            {
-                Tag = info
-            };
-        }
-
-        private VersionInfoFile ParseVersionInfo(string[] unparsedInfo)
-        {
-            return new VersionInfoFile()
-            {
-                id = unparsedInfo[0],
-                type = unparsedInfo.Length >= 3 ? unparsedInfo[2] : "unknown",
-                downloads = new VersionInfoFile.MinecraftDownloads()
-                {
-                    server = new VersionInfoFile.MinecraftDownloads.MinecraftDownloadInfo()
-                    {
-                        size = -1,
-                        url = unparsedInfo[1]
-                    }
-                }
-            };
-        }
-
-        #endregion
-
-        #region WebClient events
+        #region Downloader events
         
         private void DownloadProgressChanged(AsyncDownloadProgress progress)
         {
@@ -264,5 +158,20 @@ namespace Minecraft_Server_Downloader
         }
 
         #endregion
-    }
+        
+        #endregion
+        
+        #region Methods
+
+        private void UpdateListOfVersions(IEnumerable<VersionInfoFile> versions)
+        {
+	        var items = versions
+		        .Select(version => new ListViewItem(version.id))
+		        .ToArray();
+	        metroListView1.Clear();
+	        metroListView1.Items.AddRange(items);
+        }
+
+        #endregion
+	}
 }
