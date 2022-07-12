e348691c98ba33029e1cf2b26b75b3d77af7ae2f
RadCraftplay <radcraftplay2@gmail.com>
2022-07-12 23:55:12 +0200

    Updated AssemblyInfo.cs

--- Minecraft Version Downloader/Properties/AssemblyInfo.cs
+++ Minecraft Version Downloader/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Distroir")]
 [assembly: AssemblyProduct("Minecraft Server Downloader")]
-[assembly: AssemblyCopyright("Copyright © Distroir 2016-2020")]
+[assembly: AssemblyCopyright("Copyright © Distroir 2016-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
